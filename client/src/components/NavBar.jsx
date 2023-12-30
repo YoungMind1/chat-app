e043a394ecdcc489694da9ea6e2c2eb81a2c0d6e
Mobiwn <mobin.kh15@gmail.com>
2023-12-30 22:56:44 +0330

    Take care of further adjustment related to NavBar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import { Container, Nav, Navbar, Stack } from "react-bootstrap";
+import {Link} from "react-router-dom";
+
+const NavBar = () => {
+    return ( <Navbar bg="dark" className="mb-4" style={{ height:"3.75rem"}}>
+            <Container>
+                <h2>
+                    <Link to="/" className="link-light text-decoration-none">
+                        ChatApp
+                    </Link>
+                </h2>
+                <span className="text-warning">Logged in as Mobin</span>
+                <Nav>
+                    <Stack direction="horizontal" gap={3}>
+                        <Link to="/login" className="link-light text-decoration-none">
+                            Login
+                        </Link>
+                        <Link to="/register" className="link-light text-decoration-none">
+                            Register
+                        </Link>
+                    </Stack>
+                </Nav>
+            </Container>
+        </Navbar> );
+}
+ 
+export default NavBar;
