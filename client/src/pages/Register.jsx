6f9e27e725ab8b3cd276ccb5ad9a07370c30d09f
Mobiwn <mobin.kh15@gmail.com>
2023-12-30 23:13:46 +0330

    Finalize the design and and required logics for Register and Login pages

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,33 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Register = () => {
-    return ( <>Register</> );
+    return ( <>
+            <Form>
+                <Row style={{
+                    height: "100vh",
+                    justifyContent: "center",
+                    paddingTop: "10%"
+                }}>
+                    <Col xs={6}>
+                        <Stack gap={3}>
+                            <h2>Register</h2>
+
+                            <Form.Control type="text" placeholder="Name"/>
+                            <Form.Control type="email" placeholder="Email"/>
+                            <Form.Control type="password" placeholder="Password"/>
+                            <Button variant="primary" type="submit">
+                                Register
+                            </Button>
+
+                            <Alert variant="danger">
+                                <p>An Error occured.</p>
+                            </Alert>
+                        </Stack>
+                    </Col>
+                </Row>    
+            </Form>   
+        </>
+    );
 }
  
 export default Register;
