f8cb80f01692e2a28182fb9ba9d5379dfa040912
Mobiwn <mobin.kh15@gmail.com>
2023-12-30 22:37:52 +0330

    Add NavBar, Remove classNames and adjust the index.css file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,21 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Container} from "react-bootstrap";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-  <Container className="text-secondary">
-    <Routes>
-      <Route path="/" element={<Chat/>} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="*" element={<Navigate to="/"/>} />
-    </Routes>
-  </Container>
+    <>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Chat/>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
