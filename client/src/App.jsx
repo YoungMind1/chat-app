0071ec38b093a2bc308b5a12bf1ea1f1f8d6f317
Mobiwn <mobin.kh15@gmail.com>
2023-12-30 22:12:01 +0330

    Finish working on react-router-dom by finalizing the required code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-import{Routes, Route, Navigate} from "react-router-dom"
+import{Routes, Route, Navigate} from "react-router-dom";
+import Chat from "./pages/Chat";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <>Chat app</>
-  )
+  return <>
+  <Routes>
+    <Route path="/" element={<Chat/>} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/login" element={<Login/>} />
+    <Route path="*" element={<Navigate to="/"/>} />
+  </Routes>
+  </>
 }
 
 export default App
