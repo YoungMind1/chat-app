360f12686c6a2fb503791ed80d64973183c6c848
BradTZ8 <bardia87tz@gmail.com>
2023-12-31 18:27:23 +0330

    chatApi finished  + created router using Express.

--- server/Routes/chatRoute.js
+++ server/Routes/chatRoute.js
@@ -1,1 +1,14 @@
-const expres
+const express = require("express")
+const {
+    createChat,
+    findUserChats,
+    findChat,
+} = require("../Controllers/chatController");
+
+const router = express.Router();
+
+router.post("/", createChat);
+router.get("/:userId", findUserChats);
+router.get("/find/:firstId/:secondId", findChat);
+
+module.exports = router;
