6611e0cd31f79d94e9f3dd3e2c438c4d3ac375a2
AlisaHosseinAbadi <h.alisa81.ah@gmail.com>
2024-01-01 22:19:23 +0330

    add fetch chats

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,4 +1,12 @@
+import {useContext} from "react";
+import { ChatContext } from "../context/ChatContext";
+
 const Chat = () => {
+
+  const {userChats, isUserChatsLoading, userChatsError,} = useContext(ChatContext)
+
+  console.log("UserChats",userChats)
+
   return <>Chat</>;
 };
 
