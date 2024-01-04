3301b855052c4ba212f146d06d56872dd08eca75
arash demokri <arash.demokri84@gmail.com>
2024-01-04 04:56:54 +0330

    Add List Chats

---
+++ client/src/hooks/useFetchRecipient.js
@@ -0,0 +1,35 @@
+import{useEffect , useState }from "react";
+import { baseUrl, getRequest } from "../utils/services";
+
+export const useFetchRecipientUser = (chat , user) =>{
+    const [recipientUser, setRecipientUser]= useState(null);
+    const [error, setError]= useState(null);
+
+    const recipientID = chat?.members?.find((id) => id !== user?._id);
+
+    console.log("chat" , chat);
+
+    useEffect(() => {
+
+        const getUser = async () => {
+            if(!recipientID) return null;
+
+            const response = await getRequest(`${baseUrl}/users/find/${recipientID}` )
+
+
+            if(response.error) {
+                return setError(error);
+            }
+
+
+            setrecipientuser(response);
+        
+        };
+
+        getuser()
+
+
+    }, []);
+
+    return{recipientuser }
+};
