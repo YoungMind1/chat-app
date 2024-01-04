c671b1107a4555e8690ea2b605d7727d7e7227bf
arash demokri <arash.demokri84@gmail.com>
2024-01-04 16:46:49 +0330

    Create Chats

---
+++ client/src/components/chat/potentialChats.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { ChatContext } from "../../context/ChatContext";
+
+const PotentialChats = () => {
+    const { user } = useContext(AuthContext);
+    const { potentialChats, createChat } = useContext(ChatContext);
+  
+    console.log("pchats", potentialChats);
+  
+    return (
+      <>
+        <div className="all-users">
+          {potentialChats &&
+            potentialChats.map((u, index) => {
+              return (
+                <div 
+                  className="single-user" 
+                  key={index} 
+                  onClick={() => createChat(user._id, u._id)}
+                >
+                  {u.name}
+                  <span className="user-online"></span>
+                </div>
+              );
+            })}
+        </div>
+      </>
+    );
+  };
+  
+
+export default PotentialChats;
