8c55c60d1375c3a48df6e6145fe9d4cb02a55c3a
arash demokri <arash.demokri84@gmail.com>
2024-01-04 04:56:54 +0330

    Add List Chats

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,13 +1,32 @@
-import {useContext} from "react";
+import { useContext } from "react";
+import { Container, Stack } from "react-bootstrap";
 import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
 
-  const {userChats, isUserChatsLoading, userChatsError,} = useContext(ChatContext)
+  const { user } = useContext(AuthContext);
 
-  console.log("UserChats",userChats)
+  const { userChats, isUserChatsLoading, userChatsError } = useContext(ChatContext);
 
-  return <>Chat</>;
+
+  return (
+    <Container>
+      {userChats?.length < 1 ? null : (
+        <Stack direction="horizontal" gap={4} 
+        className="align-items-start">
+          <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+            {isUserChatsLoading && <p>loading chats...</p>}
+            {userChats?.map((chat,index)=>{
+              return(
+                <div>key={index}
+                  <userChat chat={chat} user={user}/>
+                </div>
+              )
+            })}
+          </Stack>
+          <p>ChatBox</p>
+        </Stack>
+      )}
+    </Container>
+  );
 };
-
-export default Chat;
