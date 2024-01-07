7a5c1f6d6c48cf269ab87ce70e78c93b0cf496b6
Moein RZZ <moein.rezazadeh7979@yahoo.com>
2024-01-07 17:46:38 +0330

    Display chat header and messages in ChatBox component

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
+import moment from "moment";
 
 const ChatBox = () => {
     const {user} = useContext(AuthContext)
@@ -15,12 +16,35 @@ const ChatBox = () => {
     );
 
     if (!isMessagesLoading) return(
-        <p style={{ textAlign: "center", width: "100%"}}>
+        <p style={{ textAlign: "center", width: "100%"}}> 
             Loading Chat...
         </p>
     );
 
-    return ( <>ChatBox</> );
+    return (
+        <Stack gap={4} className="chat-box">
+          <div className="chat-header">
+              <strong>{recipientUser?.name}</strong>
+          </div>
+          <Stack gap ={3} className="messages">
+            {messages && messages.map((messages, index) => (
+             <Stack 
+                key={index}  
+                className ={`${
+                    message?.senderId == user?._id
+                    ? "message self align-self-end felx-grow-0" 
+                    : "message align-self-start felx-grow-0"
+                }`}
+            >
+                <span>{message.text}</span>
+                <span className="message-footer">
+                       {moment(message.createdAt).calendar()}
+                </span>
+             </Stack>
+            ))}
+          </Stack>
+        </Stack>
+    );
 };
 
 export default ChatBox;
