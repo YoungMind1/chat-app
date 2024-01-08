f048884319c62741f0deda8c22cf39dc5f88bdd9
Moein RZZ <moein.rezazadeh7979@yahoo.com>
2024-01-08 15:05:10 +0330

    Fixing issues of sending messages
    The issue is at our ChatBox...
    We tried to call SendTextMessage again and second time we don't have UserID and text messages
    
    To fix it, we change SendTextMessage to setTextMessage in ChatBox section

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -55,7 +55,7 @@ const ChatBox = () => {
                     frontFamily="nunito"
                     borderColor="rgba(72, 112, 223, 0.2"
                 />
-                <button className="send-btn" onClick={() => sendTextMessage(textMessage, user, currentChat._id, sendTextMessage)}>
+                <button className="send-btn" onClick={() => sendTextMessage(textMessage, user, currentChat._id, setTextMessage)}>
                     <svg 
                     xmlns="http://www.w3.org/2000/svg" 
                     width="16" 
