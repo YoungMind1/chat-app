4f26f66c7461dc536147077b7601f15eee72b49a
AmirHossein <amir.1381.azhdarnzhad@gmail.com>
2024-01-14 05:49:42 +0330

    Initialize socket server and keep track of online users

---
+++ socket/index.js
@@ -0,0 +1,20 @@
+const {Server} = require("socket.io");
+
+const io = new Server({
+    cors: "http://localhost:5173"
+});
+
+let onlineUsers = [];
+io.on("connection", (socket) => {
+    console.log("new Connection: ", socket.id);
+
+    socket.on("addNewUser", (userId) => {
+        !onlineUsers.some((user) => user.userId === userId) &&
+        onlineUsers.push({
+            userId,
+            socketId: socket.id
+        });
+    })
+});
+
+io.listen(3000);
