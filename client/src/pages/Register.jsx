d3181191f9a5cd698c0c08096a3783548c3ddace
AmirHossein <amir.1381.azhdarnzhad@gmail.com>
2024-01-18 23:22:46 +0330

    Remove states from login/register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,7 +3,7 @@ import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
-  const { registerInfo, updateRegisterInfo,registerUser,registerError,isRegisterLoading} = useContext(AuthContext);
+  const {registerUser,registerError,isRegisterLoading} = useContext(AuthContext);
 
   return (
     <>
@@ -22,26 +22,14 @@ const Register = () => {
               <Form.Control
                 type="text"
                 placeholder="Name"
-                onChange={(e) =>
-                  updateRegisterInfo({ ...registerInfo, name: e.target.value })
-                }
               />
               <Form.Control
                 type="email"
                 placeholder="Email"
-                onChange={(e) =>
-                  updateRegisterInfo({ ...registerInfo, email: e.target.value })
-                }
               />
               <Form.Control
                 type="password"
                 placeholder="Password"
-                onChange={(e) =>
-                  updateRegisterInfo({
-                    ...registerInfo,
-                    password: e.target.value,
-                  })
-                }
               />
               <Button variant="primary" type="submit">
                 {isRegisterLoading?"Creating your account":"Register"}
